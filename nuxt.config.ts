4b7f968caf296d33c931ee4aa1686f824c80ecc9
Shakhawat hosen <rahad744698@gmail.com>
2026-08-11 10:07:35 +0300

    added pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
@@ -7,12 +6,72 @@ export default defineNuxtConfig({
     accessToken: process.env.JWT_ACCESS_TOKEN,
     refreshToken: process.env.JWT_REFRESH_TOKEN,
   },
-  modules: ['vuetify-nuxt-module', '@pinia/nuxt'],
+  modules: ['@vite-pwa/nuxt', 'vuetify-nuxt-module', '@pinia/nuxt'],
   vuetify: {
     vuetifyOptions: {
       theme: {
         defaultTheme: "dark"
       }
+    }
+  },
+  app: {
+  head: {
+    link: [
+      { rel: 'manifest', href: '/manifest.webmanifest' }
+    ]
+  }
+},
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'My App',
+      short_name: 'App',
+      description: 'My awesome Nuxt app',
+      theme_color: '#121212',
+      background_color: '#121212',
+      icons: [
+        { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+        { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
+        { src: 'icon-512-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+      ]
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^\/api\/.*/i,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api-cache',
+            expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 },
+            cacheableResponse: { statuses: [0, 200] },
+            networkTimeoutSeconds: 5
+          }
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+            expiration: { maxEntries: 200, maxAgeSeconds: 60 * 60 * 24 * 30 }
+          }
+        },
+        {
+          urlPattern: /\.(?:woff|woff2|ttf|otf)$/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'font-cache',
+            expiration: { maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 365 }
+          }
+        }
+      ]
+    },
+    client: {
+      installPrompt: true
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
     }
   }
 })
