0099dc157dffe48f51a11c11e00bb2b53c88172b
Shakhawat hosen <rahad744698@gmail.com>
2026-09-01 15:09:35 +0300

    updated pin

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,24 +10,27 @@
       </template>
     </div>
     <template v-else>
-      <template v-for="(value, client) of totalClients" :key="client">
+      <v-row class="my-2">
+        <v-col> Total Clients </v-col>
+        <v-col class="text-end">{{ totalClientsLength }}</v-col>
+      </v-row>
+      <template v-if="pinnedItem?.name">
         <v-list
-          class="py-0 rounded d-flex align-center justify-space-around"
-          v-if="client !== 'TestingPurpose'"
+          class="py-0 rounded d-flex align-center justify-space-around bg-secondary"
         >
           <v-list-item
             class="py-4 rounded"
             style="width: 80%"
-            :to="`/${client}`"
+            :to="`/${pinnedItem?.name}`"
           >
-            <v-list-item-title class="text-capitalize">
-              {{ client?.split(/(?=[A-Z])/).join(" ") }}
+            <v-list-item-title class="text-capitalize color-primary">
+              {{ pinnedItem?.name?.split(/(?=[A-Z])/).join(" ") }}
             </v-list-item-title>
 
             <v-list-item-subtitle>
               Total deposit:-
               {{
-                Number(value.amount).toLocaleString("bn-BD", {
+                Number(pinnedItem?.amount).toLocaleString("bn-BD", {
                   style: "currency",
                   currency: "BDT",
                   minimumFractionDigits: 0,
@@ -35,13 +38,15 @@
               }}
             </v-list-item-subtitle>
           </v-list-item>
-          <v-tooltip text="Make Favorite">
+          <v-tooltip text="Unpin">
             <template #activator="{ props }">
-              <v-btn icon variant="outlined" color="pink">
+              <v-btn icon variant="plain" color="primary" v-bind="props">
                 <v-icon
-                  @click="addToFav(client, value)"
+                  @click="addToPin('unpinned')"
                   v-bind="props"
-                  icon="mdi-heart"
+                  size="24"
+                  icon="mdi-pin-off-outline"
+                  class="text-black"
                 />
               </v-btn>
             </template>
@@ -49,17 +54,58 @@
         </v-list>
         <v-divider class="my-2" :thickness="2" />
       </template>
+      <template v-for="(value, client) of totalClients" :key="client">
+        <template
+          v-if="client !== 'TestingPurpose' && pinnedItem?.name !== client"
+        >
+          <v-list class="py-0 rounded d-flex align-center justify-space-around">
+            <v-list-item
+              class="py-4 rounded"
+              style="width: 80%"
+              :to="`/${client}`"
+            >
+              <v-list-item-title class="text-capitalize">
+                {{ client?.split(/(?=[A-Z])/).join(" ") }}
+              </v-list-item-title>
+
+              <v-list-item-subtitle>
+                Total deposit:-
+                {{
+                  Number(value.amount).toLocaleString("bn-BD", {
+                    style: "currency",
+                    currency: "BDT",
+                    minimumFractionDigits: 0,
+                  })
+                }}
+              </v-list-item-subtitle>
+            </v-list-item>
+            <v-tooltip text="Pin">
+              <template #activator="{ props }">
+                <v-btn icon variant="plain" color="primary" v-bind="props">
+                  <v-icon
+                    @click="addToPin(client, value)"
+                    v-bind="props"
+                    size="24"
+                    icon="mdi-pin-outline"
+                  />
+                </v-btn>
+              </template>
+            </v-tooltip>
+          </v-list>
+          <v-divider class="my-2" :thickness="2" />
+        </template>
+      </template>
     </template>
   </div>
   <SlowNet :slowNet="slowNet" :errMessage="errMessage" />
-  <v-snackbar v-model="addedFav.value" :timeout="10000">
+  <v-snackbar v-model="addedPin.value" :timeout="10000">
     <span class="clientName">
-      <b>{{ addedFav.name.split(/(?=[A-Z])/).join(" ") }}</b>
-      Added to Favourite
+      <b>{{ addedPin.name.split(/(?=[A-Z])/).join(" ") }}.</b>
+      <h6 class="my-0" style="color: brown">Please Refresh the Page.</h6>
     </span>
 
     <template #actions>
-      <v-icon @click="addedFav.value = false">mdi-close</v-icon>
+      <v-icon @click="addedPin.value = false">mdi-close</v-icon>
     </template>
   </v-snackbar>
 </template>
@@ -74,8 +120,11 @@ onMounted(() => {
 
 const slowNet = ref(false);
 const errMessage = ref("");
+const pinnedItem = ref(null);
+const totalClientsLength = ref(null);
 
 onMounted(async () => {
+  pinnedItem.value = JSON.parse(localStorage.getItem("pinned"));
   try {
     const res = await $fetch("/api/totalDeposits", { method: "GET" });
 
@@ -87,6 +136,7 @@ onMounted(async () => {
       }
       totalClients[field.name].amount += field.amount;
     });
+    totalClientsLength.value = Object.keys(totalClients).length - 1;
   } catch (err) {
     console.log(err, err.data);
     slowNet.value = true;
@@ -94,16 +144,23 @@ onMounted(async () => {
   }
 });
 
-const addedFav = reactive({
+const addedPin = reactive({
   value: false,
   name: null,
 });
-function addToFav(client, value) {
-  const res = useLocalStorage(client, value.amount);
-  if (res.value) {
-    addedFav.value = true;
-    addedFav.name = client;
+function addToPin(client, value) {
+  if (client === "unpinned") {
+    localStorage.removeItem("pinned");
+    addedPin.value = true;
+    addedPin.name = `${client} Remove From Pinned`;
+    return;
   }
+  localStorage.setItem(
+    "pinned",
+    JSON.stringify({ name: client, amount: value.amount }),
+  );
+  addedPin.value = true;
+  addedPin.name = `${client} Added to Pinned`;
 }
 </script>
 
